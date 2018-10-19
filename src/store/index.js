0e8584ef2d6404bf5d09a89523347dc52120529a
Rohit Sahu <robinkhan.sahu@gmail.com>
2018-10-19 19:34:31 +0530

    include student in store

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,12 @@ import Vue from "vue";
 import Vuex from "vuex";
 import user from "./modules/userModule.js";
 import school from "./modules/schoolModule.js";
+import students from "./modules/students.js";
 Vue.use(Vuex);
 export const store = new Vuex.Store({
   modules: {
     user,
-    school
+    school,
+    students
   }
 });
