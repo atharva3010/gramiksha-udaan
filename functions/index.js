4138d307051061faa6be2ad04c292eb2fece91d9
Rohit Sahu <robinkhan.sahu@gmail.com>
2019-02-04 21:21:53 +0530

    verification link generation

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,22 @@
 const functions = require('firebase-functions');
+var admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+var serviceAccount = require("./gmportal-b4054-firebase-adminsdk-2l548-03dba39b4a.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://gmportal-b4054.firebaseio.com"
+});
+var auth = admin.auth()
+exports.createUser = functions.firestore.document("unverified/{uid}").onCreate((data, context) => {
+  var newUser = data.data()
+  return auth.createUser({
+    email: newUser.email,
+    emailVerified: false,
+    disabled: false,
+  }).then(() => {
+    return auth.generateEmailVerificationLink(newUser.email)
+  }).then((link) => {
+    return
+  })
+})
